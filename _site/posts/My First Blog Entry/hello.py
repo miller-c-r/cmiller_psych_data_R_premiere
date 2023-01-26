74909aa18a83ddd69ed1d31c9e76653a538eb186
C Miller <117693746+miller-c-r@users.noreply.github.com>
2023-01-25 19:56:12 -0500

    cleaning up my qmd files, learning what qmd file is

---
+++ _site/posts/My First Blog Entry/hello.py
@@ -0,0 +1,3 @@
+print(""" Hello and Welcome to my blog on R
+I added this short python script to say hello
+I do not know how to say hello in R...yet""")
---
+++ posts/post-with-code/UntitledR.R
@@ -0,0 +1,5 @@
+library(datasets)
+
+head(precip)
+
+plot(precip, xlab="U.S. Cities", ylab="Average Rainfall in Inches")
